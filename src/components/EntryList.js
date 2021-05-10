66c6431784dd7fe3ce0ca8bec6b847782e11853b
sandy <pounoussamy.sandy@gmail.com>
2021-05-10 15:26:57 +0100

    Entries list now appear in chronological order based on implemented timestamp

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -3,13 +3,20 @@ import { Grid, Header, Divider, Container } from "semantic-ui-react";
 import Entry from "./Entry";
 
 const EntryList = ({ entries, onDelete, getEntry, onUpdate }) => {
+  const descendingOrder = entries.sort((a, b) => {
+    const dateA = Date.parse(a.timestamp);
+    const dateB = Date.parse(b.timestamp);
+
+    return dateB - dateA;
+  });
+
   return (
     <Container>
       <Grid.Row>
         <Grid.Column>
           <Header as="h1">Entries</Header>
           <Divider />
-          {entries.map((entry) => {
+          {descendingOrder.map((entry) => {
             return (
               <Entry
                 entry={entry}
