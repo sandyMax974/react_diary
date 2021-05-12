3c06c882dce512dd6cea685a31bf4c984b98e0ab
sandy <pounoussamy.sandy@gmail.com>
2021-05-12 16:03:26 +0100

    Added showAddForm function to 'ADD' button, form is now collapsing

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
 
-const AddButton = () => {
+const AddButton = ({ onAdd, showAddForm }) => {
+  const openForm = (e) => {
+    e.preventDefault();
+    onAdd(!showAddForm);
+  };
   return (
     <Button
       color="blue"
       icon="add square"
       labelPosition="right"
       content="Add"
+      onClick={(e) => openForm(e)}
     />
   );
 };
